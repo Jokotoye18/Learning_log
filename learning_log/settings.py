d1c7e03775975168dc14e271d3ad63a4f02f29cc
jokotoye18 <jokotoyeademola95@gmail.com>
2020-05-29 10:59:24 +0100

    github allauth API integration

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -59,7 +59,7 @@ INSTALLED_APPS = [
 
     ##############
     #'allauth.socialaccount.providers.facebook', 
-    #'allauth.socialaccount.providers.github', 
+    'allauth.socialaccount.providers.github', 
     'allauth.socialaccount.providers.google',
     #'allauth.socialaccount.providers.twitter', 
 
