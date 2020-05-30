f19847f8b5e388a509c392ac9cb425aec03c5a68
jokotoye18 <jokotoyeademola95@gmail.com>
2020-05-30 15:34:58 +0100

    sentry integration

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -14,9 +14,17 @@ import os
 from decouple import config
 from decouple import Csv
 import django_heroku
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+sentry_sdk.init(
+    dsn= config('SENTRY_DSN'),
+    integrations=[DjangoIntegration()],
+)
 
 
 # Quick-start development settings - unsuitable for production
